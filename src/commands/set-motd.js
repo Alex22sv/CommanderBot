6585cc26a1635e63088bc4dc9108fd1655a8f1a2
Alex22sv <00024123@uca.edu.sv>
2023-08-05 22:15:51 -0600

    Make server MOTD required for set-motd command

--- src/commands/set-motd.js
+++ src/commands/set-motd.js
@@ -17,27 +17,13 @@ module.exports = {
         .addStringOption(option => 
             option.setName('motd')
                 .setDescription('To get the MOTD ignore this field. To change the MOTD of the server send the new MOTD here.')
-                .setRequired(false)),
+                .setRequired(true)),
     async run(bot, interaction) {
         try{ 
             let name = interaction.options.getString('server');
             let serverLists = await exarotonClient.getServers();
             let server = serverLists.find(server => server.name === name || server.id === name || server.address === name);
             let newMotd = interaction.options.getString('motd');
-            if(!newMotd) {
-                let motd = await server.getMOTD();
-                let motdDecoded = motd;
-                motdDecoded = motdDecoded.replace(/\\u[\da-f]{4}/gi, c => String.fromCodePoint(parseInt(c.substr(2), 16)));
-                motdDecoded = motdDecoded.replace(/\n/g, '').replace('\\n', '\n');
-                motdDecoded = motdDecoded.replace(/§[\da-gk-or]/g, '');
-                const motdEmbed = new MessageEmbed()
-                    .setTitle(`MOTD for server ${server.name}`)
-                    .setDescription(`\`With formatting:\` ${motd} \n\`Without formatting:\` ${motdDecoded}`)
-                    .setColor(embedColor)
-                    .setTimestamp()
-                    .setFooter({text:interaction.user.tag, iconURL:interaction.user.displayAvatarURL()})
-                return interaction.reply({ embeds:[motdEmbed], ephemeral:false});
-            }
             await server.setMOTD(newMotd);
             const updatedMOTD = new MessageEmbed()
                 .setDescription(`**The MOTD for the server _${server.name}_ has changed to:** \n${newMotd}`)
